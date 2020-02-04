4c4992e7675fb6e55783dfa9179d06ddb1a4a0ba
AJ Olson <silentx.alex@gmail.com>
2020-02-04 15:43:14 -0500

    added conditional rendering for user interests

--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SecretSantaContext from '../SecretSantaContext';
+import config from '../config';
 
 class Profile extends Component {
 
     static contextType = SecretSantaContext;
 
     componentDidUpdate(prevState) {
-        if (this.context.user.userInterests !== prevState.userInterests) {}
+        if (this.context.user.userInterests !== prevState.userInterests) { }
     }
 
     addUserInterest = (interest, e) => {
@@ -20,24 +21,24 @@ class Profile extends Component {
     }
 
     compareIdToParamsPairData = (id, giftee) => {
-        let userId = id.toString()
-        if(userId === this.props.match.params.userId) {
-        return giftee.map((giftee, i) => {
-            let id = giftee.giftee_id.toString()
+        let userId = id.toString();
+        if (userId === this.props.match.params.userId) {
+            return giftee.map((giftee, i) => {
+                let id = giftee.giftee_id.toString()
 
-            return <li key={i}><Link to={id} key={i}>{giftee.giftee}</Link></li>
-        }) 
+                return <li key={i}><Link to={id} key={i}>{giftee.giftee}</Link></li>
+            })
         } else {
             return "It wouldn't be very secretive to see these pairs"
         }
     }
 
     compareIdToParamsPoolData = (id, poolData) => {
-        let userId = id.toString()
+        let userId = id.toString();
 
-        if(userId === this.props.match.params.userId) {
+        if (userId === this.props.match.params.userId) {
 
-            if(poolData.length === 0) {
+            if (poolData.length === 0) {
                 return `You haven't created any pools`
             }
             return poolData.map((pool) => {
@@ -49,12 +50,11 @@ class Profile extends Component {
         }
     }
 
-    render() {
-        const { userInterests, id, pairData, poolData } = this.context.user;
-        return (
-            <section>
-                <h1>Profile</h1>
+    compareIdToParamsAddUserInterests = (id) => {
+        let userId = id.toString();
 
+        if (userId === this.props.match.params.userId) {
+            return (
                 <form>
                     <label>Add Interests</label>
                     <input ref={HTMLInputElement => this.input = HTMLInputElement} />
@@ -63,22 +63,60 @@ class Profile extends Component {
                         this.addUserInterest(this.input.value, e)
                     }}>Add</button>
                 </form>
+            )
+        }
+    }
 
+    compareIdToParamsUserInterest = (id, userInterests) => {
+        let userId = id.toString();
+
+        if (userId === this.props.match.params.userId) {
+            return userInterests.map((interest, i) => (
+                <li key={`${interest}${i}`}>
+                    {interest.interest}
+                    <a href="/" onClick={(e) => {
+                        e.preventDefault();
+                        this.removeUserInterest(interest);
+                    }}>(X)</a>
+                </li>
+            ))
+        } else {
+            fetch(`${config.API_ENDPOINT}/interests/${id}`, {
+                method: 'GET',
+            })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error (`Something went wrong`)
+                }
+
+                return res.json()
+            })
+            .then(userInt => {
+                console.log({userInt})
+                return userInt.map((interest, i) => (
+                    <li key={`${interest}${i}`}>
+                        {interest.interest}
+                    </li>
+                ))
+            })
+        }
+    }
+
+    render() {
+        const { userInterests, id, pairData, poolData } = this.context.user;
+        return (
+            <section>
+                <h1>Profile</h1>
+
+
+                {this.compareIdToParamsAddUserInterests(id)}
                 <h3>Interests</h3>
                 <ul>
-                    {userInterests.map((interest, i) => (
-                        <li key={`${interest}${i}`}>
-                            {interest.interest}
-                            <a href="/" onClick={(e) => {
-                                e.preventDefault();
-                                this.removeUserInterest(interest);
-                            }}>(X)</a>
-                        </li>
-                    ))}
+                    {this.compareIdToParamsUserInterest(id, userInterests)}
                 </ul>
                 <h3>Pairs</h3>
                 <ul>
-                {this.compareIdToParamsPairData(id, pairData)}
+                    {this.compareIdToParamsPairData(id, pairData)}
                 </ul>
 
                 <h3>Pools</h3>
